c86e5eaf8cba249bf1f070932df09470b00443b0
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-04-28 19:28:14 +0100

    created a form with validation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
